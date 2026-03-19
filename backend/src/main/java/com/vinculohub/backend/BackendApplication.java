f58f89a8bf6f725c77a91a3294b1286b31776045
NicolasTanure <nicolastanurem@hotmail.com>
2026-03-18 21:11:41 -0300

    docs: creating MVC structure folders

--- backend/src/main/java/com/vinculohub/backend/BackendApplication.java
+++ backend/src/main/java/com/vinculohub/backend/BackendApplication.java
@@ -1,3 +1,4 @@
+/* (C)2026 */
 package com.vinculohub.backend;
 
 import org.springframework.boot.SpringApplication;
@@ -6,8 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
 }
