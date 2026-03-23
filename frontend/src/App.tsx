9a51d807855d35c31e2f7c70402dead92c4f2501
Nicolas Tanure Miszczuk <104696829+NicolasTanure@users.noreply.github.com>
2026-03-23 20:45:16 -0300

    feat: setup inicial do monorepo com frontend e backend (#2)
    * feat: setup inicial do monorepo com frontend e backend
    
    * docs: atualiza README com instruções de execução
    
    * docs: atualizando o Readme.md para incluir comando do windows
    
    * added PR template and sync to Gitlab workflow
    
    * fixed Gitlab repository url
    
    * fix: ajusta versoes do vite e react-plugin para resolver conflito com tailwind
    
    * fix: readme
    
    * test: configura ambiente de testes com junit, h2 e jacoco
    
    * docs: configura proprietarios do codigo para revisao automatica
    
    * docs: configura CODEOWNERS para revisao automatica dos owners
    
    * added .env files to avoid problems
    
    * fixed branch name on sync workflow
    
    * add: linters backend
    
    * Docker setup (#9)
    
    * feat: add dockerfile for each service and compose
    
    * docs: creating MVC structure folders
    
    * docs: creating MVC structure folders part 2
    
    * docs: melhorando o readme
    
    * feat: configure dual-database (PostgreSQL/H2) and docker-compose
    
    * feat: add front folder structure with base examples
    
    * feat: added some missing dependencies for frontend
    
    * feat: added vite base url
    
    * feat: update .gitignore to include more file types
    
    * feat: add frontend environment configuration file
    
    * feat: removendo folder view e adicionando config, dto e exception
    
    ---------
    
    Co-authored-by: MarciusMoraes <marcius.vargas@edu.pucrs.br>
    Co-authored-by: Vitor Amaral <vitor.do.amaral@sap.com>
    Co-authored-by: Luca Mandelli <luca.part.mand@gmail.com>
    Co-authored-by: João Coelho <joaoantonioscoelho@gmail.com>

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-blue-600 text-white text-4xl font-bold">
+      Tailwind Funcionando!
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,7 @@
+export const Home = () => {
+  return (
+    <main>
+      <h1>Bem-vindo ao VínculoHub Portal</h1>
+    </main>
+  );
+};
