1ee39c86146ccd55e625ed365d9e44727430eb96
joão coelho <joaoantonioscoelho@gmail.com>
2026-03-24 18:27:45 -0300

    feat: setup inicial do monorepo com frontend e backend (#2) (#20)
    * feat: setup inicial do monorepo com frontend e backend
    
    * docs: atualiza README com instruções de execução
    
    * docs: atualizando o Readme.md para incluir comando do windows
    
    * added PR template and sync to Gitlab workflow
    
    * fixed Gitlab repository url
    
    * fix: ajusta versoes do vite e react-plugin para resolver conflito com tailwind
    
    * fix: readme
    
    * test: configura ambiente de testes com junit, h2 e jacoco
    
    * docs: configura proprietarios do codigo para revisao automatica
    
    * docs: configura CODEOWNERS para revisao automatica dos owners
    
    * added .env files to avoid problems
    
    * fixed branch name on sync workflow
    
    * add: linters backend
    
    * Docker setup (#9)
    
    * feat: add dockerfile for each service and compose
    
    * docs: creating MVC structure folders
    
    * docs: creating MVC structure folders part 2
    
    * docs: melhorando o readme
    
    * feat: configure dual-database (PostgreSQL/H2) and docker-compose
    
    * feat: add front folder structure with base examples
    
    * feat: added some missing dependencies for frontend
    
    * feat: added vite base url
    
    * feat: update .gitignore to include more file types
    
    * feat: add frontend environment configuration file
    
    * feat: removendo folder view e adicionando config, dto e exception
    
    ---------
    
    Co-authored-by: Nicolas Tanure Miszczuk <104696829+NicolasTanure@users.noreply.github.com>
    Co-authored-by: MarciusMoraes <marcius.vargas@edu.pucrs.br>
    Co-authored-by: Vitor Amaral <vitor.do.amaral@sap.com>
    Co-authored-by: Luca Mandelli <luca.part.mand@gmail.com>

---
+++ backend/src/test/java/com/vinculohub/backend/BackendApplicationTests.java
@@ -0,0 +1,14 @@
+/* (C)2026 */
+package com.vinculohub.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
