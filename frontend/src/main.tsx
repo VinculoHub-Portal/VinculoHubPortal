933e5f80abe97b86bd5ddc3877397b41473595ed
MarciusMoraes <marcius.vargas@edu.pucrs.br>
2026-03-30 06:57:46 -0300

    feat: updated themes logic to work with tailwind correctly. Also organized the general styles on a styles folder

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import './styles/main.css' 
+import LandingPage from './pages/LandingPage/index' // Ajuste o caminho aqui
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <LandingPage />
   </StrictMode>,
 )
---
+++ frontend/src/pages/LandingPage/index.tsx
@@ -0,0 +1,12 @@
+function LandingPage() {
+  return (
+    /* Testando a cor customizada do main.css e o fundo */
+    <div className="flex h-screen items-center justify-center bg-vinculo-bg">
+      <h1 className="bg-vinculo-main text-white p-8 rounded-2xl shadow-2xl text-4xl font-bold">
+        VinculoHub: Tailwind v4 + Custom Theme!
+      </h1>
+    </div>
+  )
+}
+
+export default LandingPage
