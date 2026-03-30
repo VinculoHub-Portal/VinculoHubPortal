8b30fd3e1e4278a27b65c9a10244c4a6750ec473
MarciusMoraes <marcius.vargas@edu.pucrs.br>
2026-03-30 19:31:00 -0300

    fix: removed unnecessary comments

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/main.css' 
-import LandingPage from './pages/LandingPage/index' // Ajuste o caminho aqui
+import LandingPage from './pages/LandingPage/index' 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
