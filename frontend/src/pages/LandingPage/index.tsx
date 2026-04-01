3c1cdf241fde3b6765ba4aea0b8286f5ceb1391c
lucamandelli <luca.mandelli@poatek.com>
2026-04-01 20:15:11 -0300

    add initial config for landing page

---
+++ frontend/src/pages/LandingPage/index.tsx
@@ -0,0 +1,10 @@
+import { Header } from "../../components/general/Header"
+
+export default function LandingPage() {
+  return (
+    <div className="min-h-screen bg-slate-50 flex flex-col gap-10 pb-20">
+      <Header />
+      <h1>Landing Page</h1>
+    </div>
+  )
+}
