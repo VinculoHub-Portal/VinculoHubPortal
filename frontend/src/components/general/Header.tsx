8c57db0fb21b6348c35ecb7199573340588801b4
guilherme-messer <141692220+guilherme-messer@users.noreply.github.com>
2026-04-02 16:33:56 -0300

    Add TODO comment for registration button routing
    Added a TODO comment to specify routing behavior for the registration button.

--- frontend/src/components/general/Header.tsx
+++ frontend/src/components/general/Header.tsx
@@ -47,7 +47,7 @@ export function Header() {
             variant="outline"
             fullWidth
             className="border-white text-white rounded-full py-3"
-            onClick={() => navigate("/company/register")}
+            onClick={() => navigate("/company/register")} // TODO: mandar para a rota que seleciona se é cadastro de empresa ou ong 
           >
             Cadastro
           </BaseButton>
