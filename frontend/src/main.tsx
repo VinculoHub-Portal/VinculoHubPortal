2813afa48de931a5c0e64d01a43f44929bcddbfb
Enzo Picoli <enzosalatinopicoli@gmail.com>
2026-04-04 15:48:47 -0300

    fix de routing

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/main.css' 
-import ComponentsPage from './pages/ComponentsPage/index'
 import {AppRouter} from './router' 
 
 createRoot(document.getElementById('root')!).render(
