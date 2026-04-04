646cd9810ab026ccbe55f38c851a58a37d05294a
Enzo Picoli <enzosalatinopicoli@gmail.com>
2026-04-04 15:51:20 -0300

    implementacoes iniciais da pag de cadastro

--- frontend/src/router/index.tsx
+++ frontend/src/router/index.tsx
@@ -1,11 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ComponentsPage from '../pages/ComponentsPage/index';
+import RegisterPage from '../pages/Registering/register-page';
 
 export const AppRouter = () => (
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<ComponentsPage />} />
-      <Route path="/cadastro" element={<div />}/>
+      <Route path="/cadastro" element={<RegisterPage />}/>
     </Routes>
   </BrowserRouter>
 );
