5a42b49abeda404b7204f40e998ac43ec538929e
arthurbrt0 <arthur.azevedo002@edu.pucrs.br>
2026-04-04 00:04:43 -0300

    step 1 cadastro instituicao

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,17 @@
+<<<<<<< HEAD
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/main.css' 
 import ComponentsPage from './components/auth/WizardSelect.tsx' 
+=======
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./styles/main.css";
+import { AppRouter } from "./router/index";
+>>>>>>> eea9221 (step 1 cadastro instituicao)
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ComponentsPage />
+    <AppRouter />
   </StrictMode>,
-)
+);
