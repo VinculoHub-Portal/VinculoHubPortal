df408fb0f47554d2d2d24dd4341e0e244a694569
Luca Mandelli <luca.part.mand@gmail.com>
2026-04-04 17:37:12 -0300

    setup app router to define app routes (#68)

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
 import './styles/main.css' 
 import ComponentsPage from './pages/ComponentsPage/index' 
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+import "./styles/main.css"
 import { Auth0Provider } from '@auth0/auth0-react'
 
 const auth0Domain = import.meta.env.VITE_AUTH0_DOMAIN
 const auth0ClientId = import.meta.env.VITE_AUTH0_CLIENT_ID
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Auth0Provider
       domain={auth0Domain}
--- frontend/src/router/index.tsx
+++ frontend/src/router/index.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import ComponentsPage from '../pages/ComponentsPage/index';
 import RegisterPage from '../pages/Registering/register-page';
+import LandingPage from "../pages/LandingPage"
+import ComponentsPage from "../pages/ComponentsPage"
 
 export const AppRouter = () => (
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<ComponentsPage />} />
       <Route path="/cadastro" element={<RegisterPage />}/>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/components" element={<ComponentsPage />} />
     </Routes>
   </BrowserRouter>
-);
+)
