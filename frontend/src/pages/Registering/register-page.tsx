af347d6d86df65157c562ead5300523ade368546
Enzo Picoli <enzosalatinopicoli@gmail.com>
2026-04-04 15:51:20 -0300

    implementacoes iniciais da pag de cadastro

---
+++ frontend/src/pages/Registering/register-page.tsx
@@ -0,0 +1,10 @@
+export default function RegisterPage(){
+    return <body className="cadastro-body">
+        <style href="VinculoHubPortal\frontend\src\styles\main.css"/>
+        <div className="cadastro-container"> Cadastro
+            <div></div>
+            <div></div>
+            <div></div>
+             </div>
+    </body>
+};
