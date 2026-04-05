6ee2af8fb7853488b66ff42fa79f733504cc48ac
Tiago Prudencio <tiprudencio2007@gmail.com>
2026-04-05 19:05:53 -0300

    feat(login): add Auth0 provider and login button

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/main.css' 
 import ComponentsPage from './pages/ComponentsPage/index' 
+import { Auth0Provider } from '@auth0/auth0-react'
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ComponentsPage />
+    <Auth0Provider
+      domain="dev-u4fix3yzonl4bp4i.us.auth0.com"
+      clientId="8G4a34RjM1zx0QwJ1F5I4OVyEwywi6zU"
+      authorizationParams={{ redirect_uri: window.location.origin }}
+    >
+      <ComponentsPage />
+    </Auth0Provider>
   </StrictMode>,
 )
