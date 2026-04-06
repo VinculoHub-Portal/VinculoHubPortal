dc590b99ebd8c79382113a133bae180ce1f01f05
Enzo Picoli <enzosalatinopicoli@gmail.com>
2026-04-06 20:29:01 -0300

    pag de passo 4 finalizada

--- frontend/src/pages/Registering/Steps/Step4.tsx
+++ frontend/src/pages/Registering/Steps/Step4.tsx
@@ -1,6 +1,11 @@
-export function Enterprise_Registering_Step_4(cur_step: BigInteger){
+export function Enterprise_Registering_Step_4(){
 
 }
-export function NPO_Registering_Step_4(cur_step: BigInteger){
-
+export function NPO_Registering_Step_4(){
+    return <>
+    <h3>Telefone(opcional)</h3>
+    <textarea placeholder="+55 1234-5678"></textarea>
+    <h3>Endereco(opcional)</h3>
+    <textarea placeholder="rua pimentel, bandeira, enzolandia"></textarea>
+    </>
 }
