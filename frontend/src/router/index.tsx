c416435aca60bf037ac9309fd7ea57fcfe292009
LuccaFamas <Lucca.marques@edu.pucrs.br>
2026-04-06 20:37:44 -0300

    edited import at router

--- frontend/src/router/index.tsx
+++ frontend/src/router/index.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import LandingPage from "../pages/LandingPage/info_tab"
+import LandingPage from "../pages/LandingPage/"
 import ComponentsPage from "../pages/ComponentsPage"
 
 export const AppRouter = () => (
