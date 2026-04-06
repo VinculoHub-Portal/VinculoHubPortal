a19cfaf989ee8d8c17e4bfac8549b1de0533d17e
Enzo Picoli <enzosalatinopicoli@gmail.com>
2026-04-06 17:57:25 -0300

    Registering pages defined

---
+++ frontend/src/pages/Registering/Steps/Step4.tsx
@@ -0,0 +1,6 @@
+export function Enterprise_Registering_Step_4(cur_step: BigInteger){
+
+}
+export function NPO_Registering_Step_4(cur_step: BigInteger){
+
+}
--- frontend/src/pages/Registering/register-page.tsx
+++ frontend/src/pages/Registering/register-page.tsx
@@ -1,7 +1,8 @@
+import { WizardSteps } from "../../components/auth/WizardSteps";
 export default function RegisterPage(){
     return <body className="cadastro-body">
         <style href="VinculoHubPortal\frontend\src\styles\main.css"/>
-        <div className="cadastro-container"> Cadastro
+        <div className="cadastro-container"> <WizardSteps currentStep={1}></WizardSteps>Cadastro
             <div></div>
             <div></div>
             <div></div>
