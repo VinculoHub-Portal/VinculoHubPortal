6ab99b316f65af57d625cbb67c5239c6da383917
Enzo Picoli <enzosalatinopicoli@gmail.com>
2026-04-06 20:34:36 -0300

    Step4 refactor

--- frontend/src/pages/Registering/Steps/Step4.tsx
+++ frontend/src/pages/Registering/Steps/Step4.tsx
@@ -2,10 +2,30 @@ export function Enterprise_Registering_Step_4(){
 
 }
 export function NPO_Registering_Step_4(){
-    return <>
-    <h3>Telefone(opcional)</h3>
-    <textarea placeholder="+55 1234-5678"></textarea>
-    <h3>Endereco(opcional)</h3>
-    <textarea placeholder="rua pimentel, bandeira, enzolandia"></textarea>
-    </>
+    return <div className="space-y-4">
+          <h6 className="font-semibold text-[#00467F]">Informações básicas</h6>
+          <div>
+            <label className="block text-black mb-1">
+              Telefone
+              <span className="text-gray-500 ml-1">(opcional)</span>
+            </label>
+            <input
+              type="text"
+              placeholder="Digite o telefone"
+              className="w-full px-4 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400 bg-[#F3F3F5]"
+            />
+          </div>
+          <div>
+            <label className="block text-black mb-1">
+              Endereço
+              <span className="text-gray-500 ml-1">(opcional)</span>
+            </label>
+            <input
+              type="text"
+              placeholder="Digite o endereço"
+              className="w-full px-4 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400 bg-[#F3F3F5]"
+            />
+          </div>
+        </div>
+    
 }
