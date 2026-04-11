7b32ac83f0cb3a844ad6c921787f54899acc9a0a
Marcius Moraes <marcius.vargas@edu.pucrs.br>
2026-04-11 00:42:31 -0300

    Feat/wizard refactor (#81)
    * feat: wizard refactor
    
    * feat: adjust router

--- frontend/src/router/index.tsx
+++ frontend/src/router/index.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import RegisterPage from '../pages/Registering/register-page';
 import LandingPage from "../pages/LandingPage"
 import ComponentsPage from "../pages/ComponentsPage"
+import RegisterPage from "../pages/RegisterPage"
+
 
 export const AppRouter = () => (
   <BrowserRouter>
@@ -9,6 +11,7 @@ export const AppRouter = () => (
       <Route path="/cadastro" element={<RegisterPage />}/>
       <Route path="/" element={<LandingPage />} />
       <Route path="/components" element={<ComponentsPage />} />
+      <Route path="/cadastro" element={<RegisterPage />} />
     </Routes>
   </BrowserRouter>
 )
