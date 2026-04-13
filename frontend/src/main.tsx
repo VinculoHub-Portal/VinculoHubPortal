b012d3450e422dc8074a048396fa28b2315b2853
guilhermemesser <guilhermemesser@gmail.com>
2026-04-13 13:57:34 -0300

    fix input component

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,12 @@
 
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './styles/main.css' 
+import './styles/main.css'
 import { Auth0Provider } from '@auth0/auth0-react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { AppRouter } from './router'
+
+const queryClient = new QueryClient()
 
 const auth0Domain = import.meta.env.VITE_AUTH0_DOMAIN
 const auth0ClientId = import.meta.env.VITE_AUTH0_CLIENT_ID
@@ -31,7 +34,9 @@ createRoot(document.getElementById("root")!).render(
         )
       }}
     >
-      <AppRouter />
+      <QueryClientProvider client={queryClient}>
+        <AppRouter />
+      </QueryClientProvider>
     </Auth0Provider>
   </StrictMode>,
 );
