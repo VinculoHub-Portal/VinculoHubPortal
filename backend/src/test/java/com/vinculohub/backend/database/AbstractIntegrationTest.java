f5fb14d8a89c95a0b8187d0eb00f3353bb0fc09a
vitor.guttler <vitor.guttler@edu.pucrs.br>
2026-04-25 18:07:51 -0300

    Spotless

--- backend/src/test/java/com/vinculohub/backend/database/AbstractIntegrationTest.java
+++ backend/src/test/java/com/vinculohub/backend/database/AbstractIntegrationTest.java
@@ -1,7 +1,6 @@
 /* (C)2026 */
 package com.vinculohub.backend.database;
 
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
@@ -9,15 +8,15 @@ import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 
 @SpringBootTest
-//@AutoConfigureMockMvc
+// @AutoConfigureMockMvc
 @ActiveProfiles("test")
 public abstract class AbstractIntegrationTest {
 
     private static final PostgreSQLContainer<?> POSTGRES =
-        new PostgreSQLContainer<>("postgres:16-alpine")
-            .withDatabaseName("vinculohub_db")
-            .withUsername("vinculohub")
-            .withPassword("vinculohub");
+            new PostgreSQLContainer<>("postgres:16-alpine")
+                    .withDatabaseName("vinculohub_db")
+                    .withUsername("vinculohub")
+                    .withPassword("vinculohub");
 
     static {
         POSTGRES.start();
@@ -28,9 +27,7 @@ public abstract class AbstractIntegrationTest {
         registry.add("spring.datasource.url", POSTGRES::getJdbcUrl);
         registry.add("spring.datasource.username", POSTGRES::getUsername);
         registry.add("spring.datasource.password", POSTGRES::getPassword);
-        registry.add("spring.datasource.driver-class-name", () ->
-            "org.postgresql.Driver"
-        );
+        registry.add("spring.datasource.driver-class-name", () -> "org.postgresql.Driver");
 
         registry.add("spring.flyway.url", POSTGRES::getJdbcUrl);
         registry.add("spring.flyway.user", POSTGRES::getUsername);
