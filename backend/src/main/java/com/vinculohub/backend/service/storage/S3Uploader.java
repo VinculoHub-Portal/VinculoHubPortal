4ff08246a71622ec829a66024e5e8f745d72974c
Enzo Picoli <enzosalatinopicoli@gmail.com>
2026-05-02 20:24:44 -0300

    LINT: Spotless reformatted files

--- backend/src/main/java/com/vinculohub/backend/service/storage/S3Uploader.java
+++ backend/src/main/java/com/vinculohub/backend/service/storage/S3Uploader.java
@@ -1,15 +1,15 @@
+/* (C)2026 */
 package com.vinculohub.backend.service.storage;
 
+import java.io.IOException;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 import software.amazon.awssdk.core.sync.RequestBody;
-import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 import software.amazon.awssdk.services.s3.S3Client;
-
-import java.io.IOException;
-import java.util.UUID;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 @Component
 @RequiredArgsConstructor
@@ -23,13 +23,15 @@ public class S3Uploader {
     public String uploadFile(MultipartFile file, String folder) throws IOException {
         String fileName = folder + "/" + UUID.randomUUID() + "_" + file.getOriginalFilename();
 
-        PutObjectRequest putObjectRequest = PutObjectRequest.builder()
-                .bucket(bucketName)
-                .key(fileName)
-                .contentType(file.getContentType())
-                .build();
+        PutObjectRequest putObjectRequest =
+                PutObjectRequest.builder()
+                        .bucket(bucketName)
+                        .key(fileName)
+                        .contentType(file.getContentType())
+                        .build();
 
-        s3Client.putObject(putObjectRequest, 
+        s3Client.putObject(
+                putObjectRequest,
                 RequestBody.fromInputStream(file.getInputStream(), file.getSize()));
 
         return String.format("https://%s.s3.amazonaws.com/%s", bucketName, fileName);
