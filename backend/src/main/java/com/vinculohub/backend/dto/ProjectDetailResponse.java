6f900d327dd2bd9bc2e6fad198051e27c3dfb51a
Joao Paris <joao.paris04@edu.pucrs.br>
2026-05-18 20:42:19 -0300

    Criada exibição instituição responsável no detalhe do projeto e respectivos testes (Novo folder de teste DTO)

--- backend/src/main/java/com/vinculohub/backend/dto/ProjectDetailResponse.java
+++ backend/src/main/java/com/vinculohub/backend/dto/ProjectDetailResponse.java
@@ -1,6 +1,7 @@
 /* (C)2026 */
 package com.vinculohub.backend.dto;
 
+import com.vinculohub.backend.model.Project;
 import com.vinculohub.backend.model.enums.ProjectType;
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -21,4 +22,55 @@ public record ProjectDetailResponse(
         String fundraisingDeadline,
         Integer beneficiariesCount,
         String location,
-        String mainObjective) {}
+        String mainObjective,
+        ProjectResponsibleInstitutionResponse responsibleInstitution 
+                                ) {
+
+         public static ProjectDetailResponse from(Project project, List<OdsResponse> odsList) {
+    ProjectResponsibleInstitutionResponse institutionResponse = null;
+
+
+    if (project.getNpo() != null) {
+        String city = null;
+        String stateCode = null;
+        String logoUrl = null;
+
+
+        if (project.getNpo().getAddress() != null) {
+            city = project.getNpo().getAddress().getCity();
+            stateCode = project.getNpo().getAddress().getStateCode();
+        }
+        
+        if (project.getNpo().getLogoUrl()!= null){
+                logoUrl = project.getNpo().getLogoUrl();
+        }
+
+        institutionResponse = new ProjectResponsibleInstitutionResponse(
+                project.getNpo().getName(),
+                logoUrl, 
+                city,
+                stateCode,
+                project.getNpo().getDescription()
+        );
+    }
+
+    return new ProjectDetailResponse(
+            project.getId(),
+            project.getTitle(),
+            project.getDescription(),
+            project.getStatus() != null ? project.getStatus().name() : null,
+            project.getType(),
+            project.getBudgetNeeded(),
+            project.getInvestedAmount(),
+            odsList,
+            project.getStartDate(),
+            project.getEndDate(),
+            project.getFocusArea(),
+            project.getFundraisingDeadline(),
+            project.getBeneficiariesCount(),
+            project.getLocation(),
+            project.getMainObjective(),
+            institutionResponse
+    );
+}
+                                }
