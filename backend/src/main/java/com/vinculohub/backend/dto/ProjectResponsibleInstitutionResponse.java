67c797b4d437a2fb0925af678fcf6277a1e716d1
vitor.guttler <vitor.guttler@edu.pucrs.br>
2026-05-30 16:25:55 -0300

    add expose do npoId

--- backend/src/main/java/com/vinculohub/backend/dto/ProjectResponsibleInstitutionResponse.java
+++ backend/src/main/java/com/vinculohub/backend/dto/ProjectResponsibleInstitutionResponse.java
@@ -2,4 +2,9 @@
 package com.vinculohub.backend.dto;
 
 public record ProjectResponsibleInstitutionResponse(
-        String name, String logoUrl, String city, String stateCode, String description) {}
+        Integer npoId,
+        String name,
+        String logoUrl,
+        String city,
+        String stateCode,
+        String description) {}
